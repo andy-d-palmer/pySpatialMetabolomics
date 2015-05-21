2fa933551bbdef19d46b5fed487de2e729e3b639
palmer <andrew.palmer@embl.de>
2015-05-21 15:28:08 +0200

    coordinate dump added Reference implementaiton added <work in progress>

---
+++ data_coordinates.py
@@ -0,0 +1,30 @@
+import h5py
+import numpy as np
+def get_coords(hdf,index_list):
+        coords = np.zeros((len(index_list),3))
+        for k in index_list:
+            coords[k,:] = hdf['/spectral_data/'+str(k)+'/coordinates/']
+        return coords
+
+def dump_coord(hdf_filename,f_out):
+    import h5py
+    import numpy as np
+    hdf = h5py.File(hdf_filename,'r')   #Readonly, file must exist
+    index_list = map(int,hdf['/spectral_data'].keys())
+    def get_coords(hdf,index_list):
+        coords = np.zeros((len(index_list),3))
+        for k in index_list:
+            coords[k,:] = hdf['/spectral_data/'+str(k)+'/coordinates/']
+        return coords
+    coords=get_coords(hdf,index_list)
+    min_0 = min([c[0] for c in coords])
+    min_1 = min([c[1] for c in coords])
+    min_2 = min([c[2] for c in coords])
+    
+    with open(f_out,'w') as f_out:
+        for ii,coord in enumerate(coords):
+            f_out.write('{},{},{},{},\n'.format(ii,(coord[0]-min_0),(coord[1]-min_1),coord[2]-min_2))
+        
+f_in = '/Volumes/alexandr/shared/ASMS_Server_Test_Data/Ctrl3s2_SpheroidsCtrl_DHBSub_centroids_IMS.hdf5' #using a temporary hdf5 based format
+f_out = '{}/ctrl3s2_coords.txt'.format('/Users/palmer/Documents/Projects/2015/SM_development/coordinates')
+dump_coord(f_in,f_out)
