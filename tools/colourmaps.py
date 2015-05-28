6e60508869a97d46e74fc264923f46bdd50ddb8a
palmer <andrew.palmer@embl.de>
2015-05-28 15:43:47 +0200

    generate custom parula colourmap

---
+++ tools/colourmaps.py
@@ -0,0 +1,47 @@
+#define output colormap
+def make_cmap(colors,name='custom', position=None, bit=False):
+    '''
+    modified from http://schubert.atmos.colostate.edu/~cslocum/custom_cmap.html
+    make_cmap takes a list of tuples which contain RGB values. The RGB
+    values may either be in 8-bit [0 to 255] (in which bit must be set to
+    True when called) or arithmetic [0 to 1] (default). make_cmap returns
+    a cmap with equally spaced colors.
+    Arrange your tuples so that the first color is the lowest value for the
+    colorbar and the last is the highest.
+    position contains values from 0 to 1 to dictate the location of each color.
+    '''
+    import matplotlib as mpl
+    import matplotlib.cm as cm
+    import numpy as np
+    bit_rgb = np.linspace(0,1,256)
+    if position == None:
+        position = np.linspace(0,1,len(colors))
+    else:
+        if len(position) != len(colors):
+            sys.exit("position length must be the same as colors")
+        elif position[0] != 0 or position[-1] != 1:
+            sys.exit("position must start with 0 and end with 1")
+    if bit:
+        for i in range(len(colors)):
+            colors[i] = (bit_rgb[colors[i][0]],
+                         bit_rgb[colors[i][1]],
+                         bit_rgb[colors[i][2]])
+    cdict = {'red':[], 'green':[], 'blue':[]}
+    for pos, color in zip(position, colors):
+        cdict['red'].append((pos, color[0], color[0]))
+        cdict['green'].append((pos, color[1], color[1]))
+        cdict['blue'].append((pos, color[2], color[2]))
+
+    cmap = mpl.colors.LinearSegmentedColormap('custom',cdict,256)
+    cm.register_cmap(name='custom', cmap=cmap)
+    return cmap
+
+c_map = make_cmap([(53,42,135)
+,(2,104,225)
+,(16,142,210)
+,(15,174,185)
+,(101,190,134)
+,(192,188,96)
+,(255,195,55)
+,(249,251,14)],bit=True,name='parula')
+
